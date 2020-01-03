0d6fa688493772de0f823900b48e26c6975d9601
provashish95 <roytonmoy95@gmail.com>
2020-01-04 00:51:42 +0600

    This is a simple CRUD

---
+++ classes/Student.php
@@ -0,0 +1,71 @@
+<?php
+include"DB.php";
+
+class Student{
+
+	private $table = "tbl_student";
+
+	private $name;
+	private $dept;
+	private $age;
+
+	public  function setName($name){
+		$this->name = $name;
+	}
+	public  function setDept($dept){
+		$this->dept = $dept;
+	}
+
+	public  function setAge($age){
+		$this->age = $age;
+	}
+
+	public function insert(){
+		$sql = "INSERT INTO $this->table(name, dept, age)VALUES(:name, :dept, :age)";
+		$stmt = DB::prepare($sql);
+		$stmt->bindParam(':name', $this->name);
+		$stmt->bindParam(':dept', $this->dept);
+		$stmt->bindParam(':age', $this->age);
+		return $stmt->execute();
+	}
+//EDIT..........
+//EDIT..........
+	public function readById($id){
+		$sql = "SELECT * FROM $this->table WHERE id=:id";
+		$stmt = DB::prepare($sql);
+		$stmt->bindParam(':id', $id);
+		$stmt->execute();
+		return $stmt->fetch();
+	}
+	public function update($id){
+		$sql = "UPDATE $this->table SET name = :name, dept = :dept, age = :age WHERE id=:id";
+		$stmt = DB::prepare($sql);
+		$stmt->bindParam(':name', $this->name);
+		$stmt->bindParam(':dept', $this->dept);
+		$stmt->bindParam(':age', $this->age);
+		$stmt->bindParam(':id', $id);
+		return $stmt->execute();
+	}
+//EDIT..........
+//EDIT..........
+	public function readAll(){
+
+		$sql = "SELECT * FROM $this->table";
+		$stmt = DB::prepare($sql);
+		$stmt->execute();
+		return $stmt->fetchAll();
+		
+	}
+	public function delete($id){
+		$sql = "DELETE FROM $this->table WHERE id=:id";
+		$stmt = DB::prepare($sql);
+		$stmt->bindParam(':id', $id);
+		return $stmt->execute();
+		
+	}
+
+}
+
+
+
+?>
---
+++ inc/footer.php
@@ -0,0 +1,18 @@
+</div>
+
+  <footer class="footeroption">
+	  <section class="footerone">
+	  <p>Provashish Roy</p>
+	  <p>Web Development</p>
+	  <p>Daffodil Ineternational University</p>
+	  
+	  </section>
+	  <section class="footerone">
+		  <p>FB Page: facebook.com/ProDelowar</p>
+		  <p>FB Group: facebook.com/groups/PBPTBD</p>
+		  <p>Web: www.trainingWithLiveProject.com</p>
+	  </section>
+  </footer>
+
+</body>
+</html>
